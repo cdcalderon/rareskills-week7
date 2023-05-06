c2e125e1abf2bf68ef32872858519adcb9432684
carlos calderon <cdcalderon@gmail.com>
2023-05-06 14:25:52 -0500

    add TrusterLenderPool target contract

--- damn-vulnerable-defi-truster/hardhat.config.js
+++ damn-vulnerable-defi-truster/hardhat.config.js
@@ -2,5 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      { version: "0.8.16" },
+      { version: "0.7.6" },
+      { version: "0.6.6" },
+      { version: "0.5.14" },
+    ],
+  },
 };
